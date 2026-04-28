3db9b429d79715a9aed54f077cec8f1d920380fb
K899-priya <kp6273275@gmail.com>
2026-04-28 12:35:14 +0530

    +

--- src/data/FamilyTree.jsx
+++ src/data/FamilyTree.jsx
@@ -11,27 +11,27 @@ const familyTree = {
   children: [
     {
       name: "Kamla Kant Singh", img: "/s1.jpeg",
-      spouse: { name: "Ramita Devi", img: "/w1.jpeg" },
+      spouse: { name: "Ramita Devi", img: "/w1.png" },
       children: [
         { name: "Kumari Preeti", img: "/c1.jpeg" },
         { name: "Kumari Priya", img: "/c2.jpeg" },
-        { name: "Kumari Khushboo", img: "/c3.JPG" },
+        { name: "Kumari Khushboo", img: "/c3.png" },
       ],
     },
     {
-      name: "Bimal Knat Singh", img: "/s2.jpeg",
-      spouse: { name: "Anita Devi", img: "/w2.JPG" },
+      name: "Bimal Knat Singh", img: "/s2.png",
+      spouse: { name: "Anita Devi", img: "/w2.png" },
       children: [
         { name: "Kumar Gaurav", img: "/c4.jpeg" },
-        { name: "Avinav Kumar", img: "/c2.JPG" },
+        { name: "Avinav Kumar", img: "/c5.png" },
       ],
     },
     {
-      name: "Mithilesh Kant Singh", img: "/s3.jpeg",
-      spouse: { name: "Sarita Devi", img: "/w3.JPG" },
+      name: "Mithilesh Kant Singh", img: "/s3.png",
+      spouse: { name: "Sarita Devi", img: "/w3.jpeg" },
       children: [
         { name: "Amit Kumar", img: "/P2122568.JPG" },
-        { name: "Shiwani Kumari", img: "/c7.JPG" },
+        { name: "Shiwani Kumari", img: "/c7.jpeg" },
       ],
     },
   ],
